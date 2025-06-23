727a81ae9f55092905e43ed7e1f068638161d904
ShohidulProgrammer <shoikat.cse@gmail.com>
2025-06-23 15:09:29 +0600

    added exports

---
+++ lib/bottom_navigation.dart
@@ -0,0 +1,8 @@
+export 'config/bloc_provider/register_bottom_navigation_bloc_provider.dart';
+export 'config/dependency_injection/register_bottom_navigation_get_it_di.dart';
+export 'view_models/badge_count_cubit.dart';
+export 'view_models/bottom_nav_bar_cubit.dart';
+export 'views/screens/bottom_nav_scaffold.dart';
+export 'views/widgets/app_badge.dart';
+export 'views/widgets/custom_bottom_nav_bar.dart';
+export 'views/widgets/icon_with_badge.dart';
