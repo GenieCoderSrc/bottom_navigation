5dee21f4e50099dab0e7c54ff690b0228d325b1f
ShohidulProgrammer <shoikat.cse@gmail.com>
2025-07-28 20:45:15 +0600

    added all packages

--- lib/config/dependency_injection/register_bottom_navigation_get_it_di.dart
+++ lib/config/dependency_injection/register_bottom_navigation_get_it_di.dart
@@ -2,7 +2,7 @@ import 'package:bottom_navigation/view_models/badge_count_cubit.dart';
 import 'package:bottom_navigation/view_models/bottom_nav_bar_cubit.dart';
 import 'package:get_it_di_global_variable/get_it_di.dart';
 
-registerBottomNavigationGetItDI() {
+void registerBottomNavigationGetItDI() {
   /// ----------------- Bottom Navigation ------------------
 
   // View Models
